7277086f6fc8ac3cbaa2f79158cb7e286c305aa6
noxman <xxc1982@gmail.com>
2015-12-13 18:48:37 +0800

    Move setting to btoa -init-

--- ui/properties_render.py
+++ ui/properties_render.py
@@ -3,200 +3,7 @@
 #
 import bpy
 from btoa.ui import classes
-from btoa.ui.classes import PanelGroups,FilterTypes,FilterDomain,MotionblurPositon
-from bpy.props import *
-from bpy.types import PropertyGroup
-def initSceneProperties(scn):
- 
-    bpy.types.Scene.MyString = StringProperty(
-        name = "String")
-    scn['MyString'] = "Lorem ipsum dolor sit amet"
-    return
-class SceneSettingItem(bpy.types.PropertyGroup):
-    ###------Render Tab------
-    bpy.types.Scene.ArnoldEnum = bpy.props.EnumProperty(items=PanelGroups)
-    ###------Sampling-------
-    bpy.types.Scene.CameraInt = IntProperty(
-        name = "", 
-        min = 1, max = 10,
-        description = "Enter an integer",default = 3)
-    bpy.types.Scene.DiffuseInt = IntProperty(
-        name = "", 
-        min = 0, max = 10,
-        description = "Enter an integer",default = 2)
-    bpy.types.Scene.GlossyInt = IntProperty(
-        name = "", 
-        min = 0, max = 10,
-        description = "Enter an integer",default = 2)
-    bpy.types.Scene.RefractionInt = IntProperty(
-        name = "", 
-        min = 0, max = 10,
-        description = "Enter an integer",default = 2)
-    bpy.types.Scene.SSSInt = IntProperty(
-        name = "", 
-        min = 0, max = 10,
-        description = "Enter an integer",default = 2)
-    bpy.types.Scene.Volume_indirectInt = IntProperty(
-        name = "", 
-        min = 0, max = 10,
-        description = "Enter an integer",default = 2)
-    bpy.types.Scene.Lock_sampling = BoolProperty(
-        name = "Lock sampling pattern", 
-        description = "True or False?")
-    bpy.types.Scene.Use_autobump_in_sss = BoolProperty(
-        name = "Use autobump in SSS", 
-        description = "True or False?")
-    bpy.types.Scene.Clamp_sample = BoolProperty(
-        name = "Clamp sample values", 
-        description = "True or False?") 
-    bpy.types.Scene.Affect_aovs = BoolProperty(
-        name = "Affect AOVs", 
-        description = "True or False?") 
-    bpy.types.Scene.Clamp_max = FloatProperty(
-        name = "", 
-        min = 0.001, max = 100.000,
-        precision = 3,
-        step = 1,
-        description = "Enter an float",default = 10)
-    bpy.types.Scene.Filter_Types = bpy.props.EnumProperty(items=FilterTypes,default = "10")
-    bpy.types.Scene.Filter_width = FloatProperty(
-        name = "",
-        precision = 2,
-        step = 1,
-        description = "Enter an float",default = 2) 
-    bpy.types.Scene.Filter_Domain = bpy.props.EnumProperty(items=FilterDomain)
-    bpy.types.Scene.Filter_minimum = FloatProperty(
-        name = "",
-        precision = 2,
-        step = 1,
-        description = "Enter an float",default = 0) 
-    bpy.types.Scene.Filter_maximum = FloatProperty(
-        name = "",
-        precision = 2,
-        step = 1,
-        description = "Enter an float",default = 1) 
-    bpy.types.Scene.Filter_scalar_mode = BoolProperty(
-        name = "Default filter.scalar_mode", 
-        description = "True or False?") 
-    ###------Ray depth
-    bpy.types.Scene.Ray_depth_total = IntProperty(
-        name = "", 
-        min = 0, max = 16,
-        description = "Enter an integer",default = 10)
-    bpy.types.Scene.Ray_depth_diffuse = IntProperty(
-        name = "", 
-        min = 0, max = 16,
-        description = "Enter an integer",default = 1) 
-    bpy.types.Scene.Ray_depth_glossy = IntProperty(
-        name = "", 
-        min = 0, max = 16,
-        description = "Enter an integer",default = 1)           
-    bpy.types.Scene.Ray_depth_reflection = IntProperty(
-        name = "", 
-        min = 0, max = 16,
-        description = "Enter an integer",default = 2)     
-    bpy.types.Scene.Ray_depth_refraction = IntProperty(
-        name = "", 
-        min = 0, max = 16,
-        description = "Enter an integer",default = 2)  
-    bpy.types.Scene.Ray_depth_volume = IntProperty(
-        name = "", 
-        min = 0, max = 16,
-        description = "Enter an integer",default = 0)   
-    bpy.types.Scene.Ray_depth_trans_depth = IntProperty(
-        name = "", 
-        min = 0, max = 16,
-        description = "Enter an integer",default = 10)
-    bpy.types.Scene.Ray_depth_trans_threshold = FloatProperty(
-        name = "",
-        min = 0, max = 1,
-        precision = 2,
-        step = 1,
-        description = "Enter an float",default = 0.99) 
-    ###------Environment------ 
-    
-    ###------Motion blur------
-    bpy.types.Scene.Motion_blur_enable = BoolProperty(
-        name = "Enable", 
-        description = "True or False?") 
-    bpy.types.Scene.Motion_blur_deformation = BoolProperty(
-        name = "Deformation", 
-        description = "True or False?") 
-    bpy.types.Scene.Motion_blur_camera = BoolProperty(
-        name = "Camera", 
-        description = "True or False?") 
-    bpy.types.Scene.Motion_blur_keys = IntProperty(
-        name = "Keys", 
-        min = 2, max = 30,
-        description = "Enter an integer",default = 2) 
-    bpy.types.Scene.Motion_blur_position = bpy.props.EnumProperty(items=MotionblurPositon, default = "1")                                           
-    bpy.types.Scene.Motion_blur_length = FloatProperty(
-        name = "",
-        min = 0, max = 1,
-        precision = 2,
-        step = 1,
-        description = "Enter an float",default = 0.5)
-    bpy.types.Scene.Motion_blur_start = FloatProperty(
-        name = "",
-        min = 0, max = 1,
-        precision = 2,
-        step = 1,
-        description = "Enter an float",default = -0.25) 
-    bpy.types.Scene.Motion_blur_end = FloatProperty(
-        name = "",
-        min = 0, max = 1,
-        precision = 2,
-        step = 1,
-        description = "Enter an float",default = 0.25) 
-    ###------Lights------
-    bpy.types.Scene.Main_lights = FloatProperty(
-        name = "",
-        min = 0, max = 0.1,
-        precision = 3,
-        step = 1,
-        description = "Enter an float",default = 0.001) 
-    ###------Textures------
-    bpy.types.Scene.Texture_accept_unmipped = BoolProperty(
-        name = "Accept unmipped", 
-        description = "True or False?",
-        default = True) 
-    bpy.types.Scene.Texture_auto_mipmap = BoolProperty(
-        name = "Auto-mipmap", 
-        description = "True or False?",
-        default = True) 
-    bpy.types.Scene.Texture_accept_untiled = BoolProperty(
-        name = "Accept untiled", 
-        description = "True or False?",
-        default = True) 
-    bpy.types.Scene.Texture_auto_tile = BoolProperty(
-        name = "Auto-tile", 
-        description = "True or False?",
-        default = True) 
-    bpy.types.Scene.Texture_tile_size = IntProperty(
-        name = "Tile size", 
-        min = 16, max = 64,
-        description = "Enter an integer", default = 64)       
-    bpy.types.Scene.Texture_use_existing = BoolProperty(
-        name = "Use existiong.tx textures", 
-        description = "True or False?",)
-    bpy.types.Scene.Texture_max_cache = FloatProperty(
-        name = "Max cache size(MB)",
-        precision = 2,
-        step = 1,
-        description = "Enter an float", default = 1024) 
-    bpy.types.Scene.Texture_max_open = IntProperty(
-        name = "Max open files", 
-        description = "Enter an integer", default = 0)
-    bpy.types.Scene.Texture_diffuse_blur = FloatProperty(
-        name = "Diffuse blur",
-        precision = 3,
-        step = 1,
-        description = "Enter an float", default = 0.031)
-    bpy.types.Scene.Texture_glossy_blur = FloatProperty(
-        name = "Glossy blur",
-        precision = 2,
-        step = 1,
-        description = "Enter an float", default = 0) 
+
 class ARNOLD_RP_render(classes.ArnoldRenderPanel):
     bl_label = ""
     bl_options = {'HIDE_HEADER'}
@@ -209,7 +16,8 @@ class ARNOLD_tab_render(classes.ArnoldRenderPanel):
     bl_options = {'HIDE_HEADER'}
     def draw(self, context):      
         layout = self.layout
-        layout.prop(context.scene,'ArnoldEnum',expand=True)
+        scn = context.scene.arnold
+        layout.prop(scn,'ArnoldEnum',expand=True)
         #arnold_menu = context.scene.ArnoldEnum       
 
 class ARNOLD_main_sampling(classes.ArnoldRenderPanel):
@@ -219,12 +27,12 @@ class ARNOLD_main_sampling(classes.ArnoldRenderPanel):
     COMPAT_ENGINES = {'arnold_renderer'}
     @classmethod
     def poll(cls, context):
-        arnold_menu = context.scene.ArnoldEnum
+        scn = context.scene.arnold
         rd = context.scene.render
-        return (rd.use_game_engine is False) and (rd.engine in cls.COMPAT_ENGINES) and (arnold_menu in cls.tabNum)
+        return (rd.use_game_engine is False) and (rd.engine in cls.COMPAT_ENGINES) and (scn.ArnoldEnum in cls.tabNum)
     def draw(self, context):      
         layout = self.layout
-        scn = context.scene
+        scn = context.scene.arnold
         row=layout.row()
         row.label(("Camera(AA) Samples:%d") %(scn.CameraInt**2))
         row =layout.row()
@@ -291,12 +99,12 @@ class ARNOLD_main_Ray_depth(classes.ArnoldRenderPanel):
     COMPAT_ENGINES = {'arnold_renderer'}
     @classmethod
     def poll(cls, context):
-        arnold_menu = context.scene.ArnoldEnum
+        arnold_menu = context.scene.arnold.ArnoldEnum
         rd = context.scene.render
         return (rd.use_game_engine is False) and (rd.engine in cls.COMPAT_ENGINES) and (arnold_menu in cls.tabNum)
     def draw(self, context):      
         layout = self.layout 
-        scn = context.scene
+        scn = context.scene.arnold
         row = layout.row()
         row.prop(scn, 'Ray_depth_total',text = "Total")
         row = layout.row()
@@ -320,12 +128,12 @@ class ARNOLD_main_Environment(classes.ArnoldRenderPanel):
     COMPAT_ENGINES = {'arnold_renderer'}
     @classmethod
     def poll(cls, context):
-        arnold_menu = context.scene.ArnoldEnum
+        arnold_menu = context.scene.arnold.ArnoldEnum
         rd = context.scene.render
         return (rd.use_game_engine is False) and (rd.engine in cls.COMPAT_ENGINES) and (arnold_menu in cls.tabNum)
     def draw(self, context):      
         layout = self.layout 
-        scn = context.scene
+        scn = context.scene.arnold
         row = layout.row()
         row.prop(scn, 'background_set', text = "Background")
         row = layout.row()
@@ -338,12 +146,12 @@ class ARNOLD_main_Motion_blur(classes.ArnoldRenderPanel):
     COMPAT_ENGINES = {'arnold_renderer'}
     @classmethod
     def poll(cls, context):
-        arnold_menu = context.scene.ArnoldEnum
+        arnold_menu = context.scene.arnold.ArnoldEnum
         rd = context.scene.render
         return (rd.use_game_engine is False) and (rd.engine in cls.COMPAT_ENGINES) and (arnold_menu in cls.tabNum)
     def draw(self, context):      
         layout = self.layout 
-        scn = context.scene
+        scn = context.scene.arnold
         row = layout.row()
         row.prop(scn, 'Motion_blur_enable')
         col = layout.column()
@@ -380,12 +188,12 @@ class ARNOLD_main_lights(classes.ArnoldRenderPanel):
     COMPAT_ENGINES = {'arnold_renderer'}
     @classmethod
     def poll(cls, context):
-        arnold_menu = context.scene.ArnoldEnum
+        arnold_menu = context.scene.arnold.ArnoldEnum
         rd = context.scene.render
         return (rd.use_game_engine is False) and (rd.engine in cls.COMPAT_ENGINES) and (arnold_menu in cls.tabNum)
     def draw(self, context):      
         layout = self.layout 
-        scn = context.scene
+        scn = context.scene.arnold
         row = layout.row()
         row.prop(scn, 'Main_lights', text = "Low light threshold")
 class ARNOLD_main_textures(classes.ArnoldRenderPanel):
@@ -395,12 +203,12 @@ class ARNOLD_main_textures(classes.ArnoldRenderPanel):
     COMPAT_ENGINES = {'arnold_renderer'}
     @classmethod
     def poll(cls, context):
-        arnold_menu = context.scene.ArnoldEnum
+        arnold_menu = context.scene.arnold.ArnoldEnum
         rd = context.scene.render
         return (rd.use_game_engine is False) and (rd.engine in cls.COMPAT_ENGINES) and (arnold_menu in cls.tabNum)
     def draw(self, context):      
         layout = self.layout 
-        scn = context.scene
+        scn = context.scene.arnold
         row = layout.row()
         row.prop(scn, 'Texture_accept_unmipped') 
         row = layout.row()
@@ -435,9 +243,7 @@ def register():
     bpy.utils.register_class(ARNOLD_main_Motion_blur)
     bpy.utils.register_class(ARNOLD_main_lights)  
     bpy.utils.register_class(ARNOLD_main_textures)
-    bpy.utils.register_class(Arnold_panel_Button)
-    bpy.utils.register_class(SceneSettingItem)
-
+    bpy.utils.register_class(Arnold_panel_Button)  
     
 def unregister():
     bpy.utils.unregister_class(ARNOLD_RP_render) 
@@ -449,7 +255,6 @@ def unregister():
     bpy.utils.unregister_class(ARNOLD_main_lights)
     bpy.utils.unregister_class(ARNOLD_main_textures)
     bpy.utils.unregister_class(Arnold_panel_Button)
-    bpy.utils.unregister_class(SceneSettingItem)
 
-if __name__ == "__main__":
-    register()
+#if __name__ == "__main__":
+ #   register()
